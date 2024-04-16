736d8ee79096dc01a39058b187ffa3ab1db4c937
Wilson Nunes de Souza Junior <wilson@milkofstone.com>
2024-04-16 19:15:06 -0300

    Tempalte temp

--- projeto/settings.py
+++ projeto/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # A gente tem apps também
+    # Os meus apps estão definidos dentro do arquivo recipes.apps.py
+    'recipes',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,9 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+
+    ### Vai buscar o home.html dentro da pasta templates/recipes ou
+    ### na pasta base_templates/global/home.html
+    ###
+    ##return render(request, 'global/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Wilson Nunes'
+    })
+
+def contato(request):
+    return HttpResponse('Contato na View')
+
+
+def sobre(request):
+    return HttpResponse('Sobre na views.py')
+
